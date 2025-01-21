d6711fd13e27442eb72fdd9e48ce8367d79d3b83
aditya12333 <adityapande107@gmail.com>
2025-01-21 18:05:44 +0000

    data ingestion

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="MLpipeline",
+    version="0.0.0",
+    author="Aditya",
+    packages=find_packages(),
+    install_requires=[]
+    )
